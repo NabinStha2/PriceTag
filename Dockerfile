af9a281bbdda5f9161673956b24ba1f1ff6898d0
NabinStha2 <nabinstha246@gmail.com>
2024-02-11 22:23:32 +0545

    docker file updated & cors fixed

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,3 @@ FROM openjdk:17.0.2-jdk-slim
 COPY --from=build /target/pricetag-0.0.1-SNAPSHOT.jar pricetag.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","pricetag.jar"]
-
-
-
-
-WORKDIR /app
--- src/main/java/com/example/pricetag/config/SecurityConfig.java
+++ src/main/java/com/example/pricetag/config/SecurityConfig.java
@@ -21,6 +21,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -44,7 +49,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         ColorLogger.logInfo("I am inside SecurityFilterChain");
         return httpSecurity.csrf(csrf -> csrf.disable())
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(unAuthorizedException))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/verify-otp", "/api/v1/auth/forgot-password",
                                 "/api/v1/auth/verify-forgot-password-otp", "/api/v1/auth/welcome")
@@ -87,6 +92,17 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         ColorLogger.logInfo("I am inside AuthenticationManager");
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
